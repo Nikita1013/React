28906ba715ea5bf294871644c2e4527122599dc4
Nikita1013 <shindenikita646@gmail.com>
2024-08-15 19:19:12 +0530

    React Project 6

---
+++ React-Project-6/src/pages/Dashboard/InfoCard.jsx
@@ -0,0 +1,27 @@
+import { Tag, Text } from "@chakra-ui/react";
+import { CustomCard } from "../../chakra/CustomCard";
+
+const InfoCard = ({ imgUrl, text, tagText, inverted }) => {
+    return(
+        <CustomCard 
+        bgImage={imgUrl} 
+        bgSize="cover"
+        bgColor={inverted ? "p.purple" : "white"} 
+        bgRepeat="no-repeat">
+            <Tag
+            color={inverted ? "p.purple" : "white"}
+            bg={ inverted ? "white" : "p.purple"}
+            borderRadius="full"
+            >
+                {tagText}
+            </Tag>
+            <Text textStyle="h5" mt={4} fontWeight="medium"
+            color={inverted ? "white" : "black.80"}
+            >
+                {text}
+            </Text>
+        </CustomCard>
+    );
+};
+
+export default InfoCard;
